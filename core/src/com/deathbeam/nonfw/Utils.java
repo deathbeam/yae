4bd3a5d66009ee2101cf1274a4e85b3a74676b7a
PandaCoder <rjlodico@ncsu.edu>
2014-11-08 21:37:22 -0500

    Removed string comparison, added string repetition

--- core/src/com/deathbeam/nonfw/Utils.java
+++ core/src/com/deathbeam/nonfw/Utils.java
@@ -192,4 +192,10 @@ public class Utils {
         in.close();
         return sBuffer.toString();
     }
+    
+    public static String repeat(String str, int n) {
+    	String out = "";
+    	for (int i = 0; i < n; i++) { out += str; }
+    	return out;
+    }
 }
