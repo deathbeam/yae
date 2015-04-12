1c4bf576739c95e66fcdd0b55d702ded7eab1be2
Thomas Slusny <slusnucky@gmail.com>
2015-04-12 21:23:27 +0200

    Fixed icon resizing, modified install script

--- install.bat
+++ install.bat
@@ -3,8 +3,9 @@ setlocal
 call javac lib/launcher/*.java
 call jar cvfe bin/non.jar Main -C lib/launcher/ .
 call jar fu bin/non.jar lib/gradle/* lib/platforms/* lib/resources/* lib/VERSION
-call luarocks make non-dev-1.rockspec
 cd lib/thirdparty
 call gradlew.bat install
 cd ../core
 call gradlew.bat install
+cd ../../
+call luarocks make non-dev-1.rockspec
