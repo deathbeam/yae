9392ab8dff4549818e294a19fc350516ba60b0ff
Thomas Slusny <slusnucky@gmail.com>
2015-04-12 20:36:28 +0200

    Moved third-party to sub directory

--- install.bat
+++ install.bat
@@ -4,5 +4,7 @@ call javac lib/launcher/*.java
 call jar cvfe bin/non.jar Main -C lib/launcher/ .
 call jar fu bin/non.jar lib/gradle/* lib/platforms/* lib/resources/* lib/VERSION
 call luarocks make non-dev-1.rockspec
-cd lib/core
+cd lib/thirdparty
+call gradlew.bat install
+cd ../core
 call gradlew.bat install
