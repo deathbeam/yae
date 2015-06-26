b075778c5bb7932cc4213ed62a79f2a2611acb8f
Thomas Slusny <slusnucky@gmail.com>
2015-06-26 23:07:30 +0200

    Added linter for projects

--- core/core/precompile.sh
+++ core/core/precompile.sh
@@ -2,6 +2,7 @@
 cd ../src
 moonc .
 cd ../../assets
+moonc -l .
 moonc -t ../.non/src .
 cd ../.non
 java -cp libs/bcel.jar:libs/luaj.jar luajc -r -d core/build/classes/main src/
--- res/lint_config.lua
+++ res/lint_config.lua
@@ -3,8 +3,7 @@ return {
     ["."] = {
       "non",
       "config",
-      "java",
-      "luajava"
+      "java"
     }
   }
 }
