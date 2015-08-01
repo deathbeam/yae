32d52f768557551bc2466a270f9e6bd7f90218cb
Thomas Slusny <slusnucky@gmail.com>
2015-08-02 01:33:03 +0200

    Fixed rocks integration and continual builds speed

--- core/shared/precompile.bat
+++ core/shared/precompile.bat
@@ -1,7 +1,7 @@
 @if "%DEBUG%" == "" @echo off
 setlocal
-cd ..\..\assets
-call moonc -t ..\.non\shared\build\lua .
-cd ..\.non
+cd build\moon
+call moonc -t ..\lua .
+cd ..\..\
 call java -cp libs\bcel.jar;libs\luaj.jar luajc -r -d shared\build\classes\main shared\build\lua\
 endlocal
