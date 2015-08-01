ca49a456381c9f12f91faa3c8e9db25cc118be05
Thomas Slusny <slusnucky@gmail.com>
2015-08-02 01:42:12 +0200

    Final fixes for rock integration

--- core/shared/precompile.bat
+++ core/shared/precompile.bat
@@ -3,5 +3,5 @@ setlocal
 cd build\moon
 call moonc -t ..\lua .
 cd ..\..\
-call java -cp libs\bcel.jar;libs\luaj.jar luajc -r -d shared\build\classes\main shared\build\lua\
+call java -cp ..\libs\bcel.jar;..\libs\luaj.jar luajc -r -d build\classes\main build\lua\
 endlocal
